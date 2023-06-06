f01aa34cd891ec92cbd70c8f7679e1e0bc3c59d0
serchg0george <serchg0george@gmail.com>
2023-06-06 21:35:56 +0300

    Add deleteById method for customer

--- src/main/java/com/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/com/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -80,5 +80,10 @@ public class CustomerServiceImpl implements CustomerService {
         customerMap.put(existing.getId(), existing);
     }
 
+    @Override
+    public void deleteById(UUID customerId) {
+        customerMap.remove(customerId);
+    }
+
 
 }
