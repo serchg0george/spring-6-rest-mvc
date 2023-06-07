c08b1fdc715e359d4ed18db45e58b260f098eae6
serchg0george <serchg0george@gmail.com>
2023-06-07 12:04:31 +0300

    Replaced JUnit test of method get to the Mockito test for method get

--- src/test/java/com/springframework/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/springframework/spring6restmvc/controllers/BeerControllerTest.java
@@ -1,22 +1,35 @@
 package com.springframework.spring6restmvc.controllers;
 
+import com.springframework.spring6restmvc.services.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@SpringBootTest
+
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
 
+    @MockBean
+    BeerService beerService;
     @Test
-    void getBeerById() {
+    void getBeerById() throws Exception {
 
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
-
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
