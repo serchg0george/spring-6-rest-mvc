149392800c79fd6467620be1ca1a04d32016e4cc
serchg0george <serchg0george@gmail.com>
2023-06-08 15:45:26 +0300

    Add tests testListBeers(), testEmptyList()

---
+++ src/test/java/com/springframework/spring6restmvc/controllers/BeerControllerIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.springframework.spring6restmvc.controllers;
+
+import com.springframework.spring6restmvc.models.BeerDTO;
+import com.springframework.spring6restmvc.repositories.BeerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BeerControllerIntegrationTest {
+    @Autowired
+    BeerController beerController;
+    
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
