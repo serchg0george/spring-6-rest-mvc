582e8edefbec57d1169a954fcad2ab952348b412
serchg0george <serchg0george@gmail.com>
2023-06-08 21:08:50 +0300

    Refactor patchBeerById() add return statement

--- src/main/java/com/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -113,7 +113,7 @@ public class BeerServiceImpl implements BeerService {
     }
 
     @Override
-    public void patchBeerById(UUID beerId, BeerDTO beer) {
+    public Optional<BeerDTO> patchBeerById(UUID beerId, BeerDTO beer) {
         BeerDTO existing = beerMap.get(beerId);
 
         if (StringUtils.hasText(beer.getBeerName())) {
@@ -135,5 +135,7 @@ public class BeerServiceImpl implements BeerService {
         if (StringUtils.hasText(beer.getUpc())) {
             existing.setUpc(beer.getUpc());
         }
+
+        return Optional.of(existing);
     }
 }
