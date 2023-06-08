d98a3aa780f63f0b85c33eee274a9b71a85e4823
serchg0george <serchg0george@gmail.com>
2023-06-08 12:26:06 +0300

    Create BeerRepository extends JpaRepo<Beer, UUID>

---
+++ src/main/java/com/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.springframework.spring6restmvc.repositories;
+
+import com.springframework.spring6restmvc.entities.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+}
