83a5f3cb9896056b33e710721f2d61da8c51a625
serchg0george <serchg0george@gmail.com>
2023-06-08 16:12:04 +0300

    Add testListCustomers(), testEmptyList()

---
+++ src/test/java/com/springframework/spring6restmvc/controllers/CustomerControllerIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.springframework.spring6restmvc.controllers;
+
+import com.springframework.spring6restmvc.entities.Customer;
+import com.springframework.spring6restmvc.models.CustomerDTO;
+import com.springframework.spring6restmvc.repositories.CustomerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class CustomerControllerIntegrationTest {
+    @Autowired
+    CustomerController customerController;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+
+
+    @Test
+    void testListCustomers() {
+        List<CustomerDTO> dtos = customerController.listCustomer();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        customerRepository.deleteAll();
+        List<CustomerDTO> dtos = customerController.listCustomer();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
