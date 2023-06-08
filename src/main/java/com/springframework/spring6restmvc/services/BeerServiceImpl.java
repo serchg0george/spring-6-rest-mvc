a25756b32eadff2c424d21bd487bfc9175744f5f
serchg0george <serchg0george@gmail.com>
2023-06-08 19:07:51 +0300

    Refactor deleteById() add return statement

--- src/main/java/com/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -106,8 +106,10 @@ public class BeerServiceImpl implements BeerService {
     }
 
     @Override
-    public void deleteById(UUID beerId) {
+    public Boolean deleteById(UUID beerId) {
         beerMap.remove(beerId);
+
+        return true;
     }
 
     @Override
