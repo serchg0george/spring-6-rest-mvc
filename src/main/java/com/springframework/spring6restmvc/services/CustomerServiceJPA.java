99f6bcc2b4ca5a43b90834343d1e5668fa9da395
serchg0george <serchg0george@gmail.com>
2023-06-08 15:44:49 +0300

    Add implementation to listCustomers(), getCustomerById()

---
+++ src/main/java/com/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,48 @@
+package com.springframework.spring6restmvc.services;
+
+import com.springframework.spring6restmvc.mappers.CustomerMapper;
+import com.springframework.spring6restmvc.models.CustomerDTO;
+import com.springframework.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+    @Override
+    public List<CustomerDTO> listCustomers() {
+        return null;
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+}
