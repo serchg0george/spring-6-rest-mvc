59b675ce151e10f801d62a27f75dc03273c2dbb6
serchg0george <serchg0george@gmail.com>
2023-06-08 20:38:46 +0300

    Implement patchCustomerById()

--- src/main/java/com/springframework/spring6restmvc/services/CustomerServiceJPA.java
+++ src/main/java/com/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -65,7 +65,17 @@ public class CustomerServiceJPA implements CustomerService {
     }
 
     @Override
-    public void patchCustomerById(UUID customerId, CustomerDTO customer) {
+    public Optional<CustomerDTO> patchCustomerById(UUID customerId, CustomerDTO customer) {
+        AtomicReference<Optional<CustomerDTO>> atomicReference = new AtomicReference<>();
 
+        customerRepository.findById(customerId).ifPresentOrElse(foundCustomer -> {
+            foundCustomer.setCustomerName(customer.getCustomerName());
+            atomicReference.set(Optional.of(customerMapper
+                    .customerToCustomerDto(customerRepository.save(foundCustomer))));
+        }, () -> {
+            atomicReference.set(Optional.empty());
+        });
+
+        return atomicReference.get();
     }
 }
