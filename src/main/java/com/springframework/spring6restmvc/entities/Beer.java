ebb8861f70217c0a8f9c0bda0de588155beda374
serchg0george <serchg0george@gmail.com>
2023-06-09 12:32:40 +0300

    Add annotations to the other properties

--- src/main/java/com/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/com/springframework/spring6restmvc/entities/Beer.java
@@ -2,6 +2,8 @@ package com.springframework.spring6restmvc.entities;
 
 import com.springframework.spring6restmvc.models.BeerStyle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -26,10 +28,18 @@ public class Beer {
     @Version
     private Integer version;
 
+    @NotNull
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyle beerStyle;
+
+    @NotNull
+    @NotBlank
     private String upc;
     private Integer quantityOnHand;
+
+    @NotNull
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
