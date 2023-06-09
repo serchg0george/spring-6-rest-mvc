8a05d883c919cb358ff5e9edeab3266710591329
serchg0george <serchg0george@gmail.com>
2023-06-09 12:22:11 +0300

    Add testUpdateBeerBlankBeerName()

--- src/test/java/com/springframework/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/springframework/spring6restmvc/controllers/BeerControllerTest.java
@@ -59,16 +59,35 @@ class BeerControllerTest {
 
 
     @Test
+    void testUpdateBeerBlankBeerName() throws Exception {
+        BeerDTO beer = beerServiceImpl.listBeers().get(0);
+        beer.setBeerName("");
+        given(beerService.updateBeerById(any(), any())).willReturn(Optional.of(beer));
+
+        MvcResult mvcResult = mockMvc.perform(put(BeerController.BEER_PATH_ID, beer.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.length()", is(1)))
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
     void testCreateBeerNullBeerName() throws Exception {
         BeerDTO beerDTO = BeerDTO.builder().build();
 
         given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(beerServiceImpl.listBeers().get(1));
 
-        MvcResult mvcResult = mockMvc.perform(post(BeerController.BEER_PATH_ID)
-                        .contentType(MediaType.APPLICATION_JSON)
+        MvcResult mvcResult = mockMvc.perform(post(BeerController.BEER_PATH)
                         .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(beerDTO)))
-                .andExpect(status().isBadRequest()).andReturn();
+                        .andExpect(status().isBadRequest())
+                        .andExpect(jsonPath("$.length()", is(6)))
+                        .andReturn();
 
         System.out.println(mvcResult.getResponse().getContentAsString());
     }
