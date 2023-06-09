fe51f215a7f01f04426a6522cd88c10d46fad38e
serchg0george <serchg0george@gmail.com>
2023-06-09 12:32:24 +0300

    Refactor testSaveBeer()

--- src/test/java/com/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.springframework.spring6restmvc.repositories;
 
 import com.springframework.spring6restmvc.entities.Beer;
+import com.springframework.spring6restmvc.models.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,8 +19,13 @@ class BeerRepositoryTest {
     @Test
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
-                .beerName("My Beer")
+                        .beerName("My Beer")
+                        .beerStyle(BeerStyle.ALE)
+                        .upc("123123123")
+                        .price(new BigDecimal("11.25"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
