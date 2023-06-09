2f58dba52a9cec3f7d3dee27eaab9288a2381a21
serchg0george <serchg0george@gmail.com>
2023-06-09 18:42:41 +0300

    Add @JdbcTypeCode to id, add number of symbols to varchar in @Column

--- src/main/java/com/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/com/springframework/spring6restmvc/entities/Beer.java
@@ -7,7 +7,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.validator.constraints.Length;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -24,7 +25,8 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
@@ -40,7 +42,7 @@ public class Beer {
 
     @NotNull
     @NotBlank
-    @Size(max = 255)
+    @Size(max = 255 )
     private String upc;
     private Integer quantityOnHand;
 
