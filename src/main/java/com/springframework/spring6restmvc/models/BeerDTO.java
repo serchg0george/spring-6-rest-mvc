f791e112d3512051d5002a12beb45a172febe2ac
serchg0george <serchg0george@gmail.com>
2023-06-09 11:02:51 +0300

    Add @NotBlank @NotNull annotations

--- src/main/java/com/springframework/spring6restmvc/models/BeerDTO.java
+++ src/main/java/com/springframework/spring6restmvc/models/BeerDTO.java
@@ -1,5 +1,7 @@
 package com.springframework.spring6restmvc.models;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,6 +15,9 @@ import java.util.UUID;
 public class BeerDTO {
     private UUID id;
     private Integer version;
+
+    @NotBlank
+    @NotNull
     private String beerName;
     private BeerStyle beerStyle;
     private String upc;
