f37547213b93b0328ce31a6d21fabe9458980a56
serchg0george <serchg0george@gmail.com>
2023-06-11 21:57:25 +0300

    Add findAllByBeerStyle(),findAllByBeerNameIsLikeIgnoreCase()

--- src/main/java/com/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/com/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -1,10 +1,16 @@
 package com.springframework.spring6restmvc.repositories;
 
 import com.springframework.spring6restmvc.entities.Beer;
+import com.springframework.spring6restmvc.models.BeerStyle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
+
+    List<Beer> findAllByBeerStyle(BeerStyle beerStyle);
 
 }
