928a1e7010a8d6eb8d52deddedf88995a5bdc6ad
serchg0george <serchg0george@gmail.com>
2023-06-11 21:58:19 +0300

    Add beerStyle param

--- src/main/java/com/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/com/springframework/spring6restmvc/services/BeerService.java
@@ -1,13 +1,14 @@
 package com.springframework.spring6restmvc.services;
 
 import com.springframework.spring6restmvc.models.BeerDTO;
+import com.springframework.spring6restmvc.models.BeerStyle;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BeerService {
-    List<BeerDTO> listBeers(String beerName);
+    List<BeerDTO> listBeers(String beerName, BeerStyle beerStyle);
 
     Optional<BeerDTO> getBeerById(UUID id);
 
