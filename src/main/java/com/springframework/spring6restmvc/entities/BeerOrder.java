6c9636b3162b6099c3e4e3e8704372786b6d4a79
serchg0george <serchg0george@gmail.com>
2023-06-12 22:36:28 +0300

    Add beerOrderShipment

--- src/main/java/com/springframework/spring6restmvc/entities/BeerOrder.java
+++ src/main/java/com/springframework/spring6restmvc/entities/BeerOrder.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 public class BeerOrder {
 
     public BeerOrder(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, String customerRef,
-                     Customer customer, Set<BeerOrderLine> beerOrderLines) {
+                     Customer customer, Set<BeerOrderLine> beerOrderLines, BeerOrderShipment beerOrderShipment) {
         this.id = id;
         this.version = version;
         this.createdDate = createdDate;
@@ -28,6 +28,7 @@ public class BeerOrder {
         this.customerRef = customerRef;
         this.setCustomer(customer);
         this.beerOrderLines = beerOrderLines;
+        this.beerOrderShipment = beerOrderShipment;
     }
 
     @Id
@@ -66,4 +67,8 @@ public class BeerOrder {
 
     @OneToMany(mappedBy = "beerOrder")
     private Set<BeerOrderLine> beerOrderLines;
+
+
+    @OneToOne
+    private BeerOrderShipment beerOrderShipment;
 }
