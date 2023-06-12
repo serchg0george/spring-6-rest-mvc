dd8888a1941c34323e19a448053c50599febfd5a
serchg0george <serchg0george@gmail.com>
2023-06-12 22:13:47 +0300

    Add @JoinTable categories,addCategory(),removeCategory()

--- src/main/java/com/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/com/springframework/spring6restmvc/entities/Beer.java
@@ -14,6 +14,7 @@ import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -45,19 +46,36 @@ public class Beer {
 
     @NotNull
     @NotBlank
-    @Size(max = 255 )
+    @Size(max = 255)
     private String upc;
     private Integer quantityOnHand;
 
     @NotNull
     private BigDecimal price;
 
+    @OneToMany(mappedBy = "beer")
+    private Set<BeerOrderLine> beerOrderLines;
+
+    @Builder.Default
+    @ManyToMany
+    @JoinTable(name = "beer_category",
+            joinColumns = @JoinColumn(name = "beer_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
+    public void addCategory(Category category) {
+        this.categories.add(category);
+        category.getBeers().add(this);
+    }
+
+    public void removeCategory(Category category) {
+        this.categories.remove(category);
+        category.getBeers().remove(this);
+    }
     @CreationTimestamp
     private LocalDateTime createdDate;
 
     @UpdateTimestamp
     private LocalDateTime updateDate;
 
-    @OneToMany(mappedBy = "beer")
-    private Set<BeerOrderLine> beerOrderLines;
 }
