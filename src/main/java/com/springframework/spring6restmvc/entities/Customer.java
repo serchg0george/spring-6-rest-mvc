83731f564a4a3e259c130e6fdbde13449c7e34b3
serchg0george <serchg0george@gmail.com>
2023-06-12 18:48:55 +0300

    Add beerOrders

--- src/main/java/com/springframework/spring6restmvc/entities/Customer.java
+++ src/main/java/com/springframework/spring6restmvc/entities/Customer.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -30,7 +31,9 @@ public class Customer {
 
     @Version
     private Integer version;
-
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<BeerOrder> beerOrders;
 }
