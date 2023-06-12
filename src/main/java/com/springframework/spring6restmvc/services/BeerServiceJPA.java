ab4bd9ce9f2e89faa8d2903af5070a63c8db9ce9
serchg0george <serchg0george@gmail.com>
2023-06-12 15:05:10 +0300

    Add buildPageRequest()

--- src/main/java/com/springframework/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/com/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -23,6 +23,10 @@ public class BeerServiceJPA implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    private final static int DEFAULT_PAGE = 0;
+    private final static int DEFAULT_PAGE_SIZE = 1;
+
+
     @Override
     public List<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory) {
 
@@ -46,6 +50,28 @@ public class BeerServiceJPA implements BeerService {
         return beerList.stream()
                 .map(beerMapper::beerToBeerDto)
                 .collect(Collectors.toList());
+    }
+
+    public PageRequest buildPageRequest(Integer pageNumber, Integer pageSize) {
+        int queryPageNumber;
+        int queryPageSize;
+
+        if (pageNumber != null && pageNumber > 0) {
+            queryPageNumber = pageNumber - 1;
+        } else {
+            queryPageNumber = DEFAULT_PAGE;
+        }
+
+        if (pageSize == null) {
+            queryPageSize = DEFAULT_PAGE_SIZE;
+        } else {
+            if (pageSize > 1000) {
+                queryPageSize = 1000;
+            } else {
+                queryPageSize = pageSize;
+            }
+        }
+        return PageRequest.of(queryPageNumber, queryPageSize);
     }
 
     public List<Beer> listBeersByNameAndStyle(String beerName, BeerStyle beerStyle) {
