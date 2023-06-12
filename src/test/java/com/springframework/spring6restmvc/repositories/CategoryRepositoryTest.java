a85e3d5740709c17a3a853adf779e64f5f52cd75
serchg0george <serchg0george@gmail.com>
2023-06-12 22:15:35 +0300

    Create CategoryRepositoryTest,add testAddCategory()

---
+++ src/test/java/com/springframework/spring6restmvc/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.springframework.spring6restmvc.repositories;
+
+import com.springframework.spring6restmvc.entities.Beer;
+import com.springframework.spring6restmvc.entities.Category;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CategoryRepositoryTest {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testBeer = beerRepository.findAll().get(0);
+    }
+
+    @Transactional
+    @Test
+    void testAddCategory() {
+        Category savedCategory = categoryRepository.save(Category.builder()
+                        .description("Ales")
+                .build());
+
+        testBeer.addCategory(savedCategory);
+        Beer savedBeer = beerRepository.save(testBeer);
+
+        System.out.println(savedBeer.getBeerName());
+    }
+}
